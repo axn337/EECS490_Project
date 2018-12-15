f67a8366e7377934c399a9931ffd3709bf623d70
axn337 <axn337@case.edu>
2018-12-15 14:24:42 -0500

    movement of camera object

--- simulator_pkg/src/image_saver.cpp
+++ simulator_pkg/src/image_saver.cpp
@@ -7,7 +7,10 @@
 #include <reconstruction/ImageStamped.h>
 #include <cstdlib>
 
-using namespace std;
+#include <Eigen/Geometry>
+#include <Eigen/Dense>
+
+using namespace Eigen;
 
 sensor_msgs::Image image_sub_;
 geometry_msgs::Pose image_pose_;
@@ -28,14 +31,14 @@ int main(int argc, char** argv) {
 	ros::Publisher my_pub_ = n.advertise<reconstruction::ImageStamped>("image_stamped", 1);
 	ros::ServiceClient client = n.serviceClient<gazebo_msgs::SetModelState>("/gazebo/set_model_state"); 
 
-	geometry_msgs::Pose start_pose;
-	start_pose.position.x = 0;
-	start_pose.position.x = 0;
-	start_pose.position.z = 0;
-	start_pose.orientation.x = 0.0;
-	start_pose.orientation.y = 0.0;
-	start_pose.orientation.z = 0.0;
-	start_pose.orientation.w = 0.0;
+	//geometry_msgs::Pose start_pose;
+	image_pose_.position.x = 0;
+	image_pose_.position.x = 0;
+	image_pose_.position.z = 0;
+	image_pose_.orientation.x = 0.0;
+	image_pose_.orientation.y = 0.0;
+	image_pose_.orientation.z = 0.0;
+	image_pose_.orientation.w = 0.0;
 
 	geometry_msgs::Twist start_twist;
 	start_twist.linear.x = 0.0;
@@ -51,34 +54,61 @@ int main(int argc, char** argv) {
 	modelstate.model_name = "my_object"; 
 	modelstate.reference_frame = "world";
 	modelstate.twist = start_twist;
-	modelstate.pose = start_pose;
+	modelstate.pose = image_pose_;
 	setmodelstate.request.model_state = modelstate;
 
-	float alpha;//orientation in x,y plane
+	float alpha=0.0;//orientation in x,y plane
 	float theta;
-	float radios;
-	float hight;
-	hight=0.3;
-	radios=0.5;
-	//theta=atan2(radios/hight);
-	alpha=0.0;
+	float radios=0.5;
+	float hight=0.3;
+	float z_bias=0.5;
+	float r_bias=0.0;
+	float p_bias=1.5708;
+	float y_bias=1.5708;
+	float x,y,z,roll,pitch,yaw;
+
 	
-	//x=sin(alpha)
-	//y=cos(alpha)
+	theta=atan2(radios,hight);
+	
+	Quaternionf q;
+
 	while (ros::ok()) {
 
+		alpha=alpha+0.174533;
+
+		x=sin(alpha);
+		y=cos(alpha);
+		z=hight-z_bias;
+		roll=x*(theta+1.5708-r_bias);
+		pitch=y*(theta+1.5708-p_bias);
+		yaw=alpha+1.5708-y_bias;
+
+	
+		q = AngleAxisf(roll, Vector3f::UnitX())
+		    * AngleAxisf(pitch, Vector3f::UnitY())
+		    * AngleAxisf(yaw, Vector3f::UnitZ());
+
+		image_pose_.position.x = x;
+		image_pose_.position.x = y;
+		image_pose_.position.z = z;
+		image_pose_.orientation.x = q.x();
+		image_pose_.orientation.y = q.y();
+		image_pose_.orientation.z = q.z();
+		image_pose_.orientation.w = q.w();
+
 		modelstate.twist = start_twist;
-		modelstate.pose = start_pose;
+		modelstate.pose = image_pose_;
 		setmodelstate.request.model_state = modelstate;
-		image_pose_=start_pose;
+
 		if (client.call(setmodelstate)){
 			ROS_INFO("New Camera Pose!!!");
-			ROS_INFO("%f, %f, %f", modelstate.pose.position.x,modelstate.pose.position.y,modelstate.pose.position.y);
+			ROS_INFO("Position: \n %f, %f, %f", modelstate.pose.position.x, modelstate.pose.position.y, modelstate.pose.position.z);
+			ROS_INFO("Orientation: \n %f, %f, %f, %f", modelstate.pose.orientation.x, modelstate.pose.orientation.y, modelstate.pose.orientation.z, modelstate.pose.orientation.w);
 		}else{
 			ROS_ERROR("Failed to call service ");
 		}
 		ros::spinOnce();
-		image_pose_=start_pose;
+
 		msg.header.stamp = ros::Time::now();
 		msg.header.frame_id = "/world";
 		msg.image=image_sub_;
