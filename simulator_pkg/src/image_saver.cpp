31d128b238da1a48199c36667f50ee4614a42f51
axn337 <axn337@case.edu>
2018-12-15 13:18:02 -0500

    building block of image saver

--- simulator_pkg/src/image_saver.cpp
+++ simulator_pkg/src/image_saver.cpp
@@ -1,133 +1,89 @@
-//get images from topic "simple_camera/image_raw"; remap, as desired;
-//search for red pixels;
-// convert (sufficiently) red pixels to white, all other pixels black
-// compute centroid of red pixels and display as a blue square
-// publish result of processed image on topic "/image_converter/output_video"
-#include <ros/ros.h>
-#include <image_transport/image_transport.h>
-#include <cv_bridge/cv_bridge.h>
-#include <sensor_msgs/image_encodings.h>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/highgui/highgui.hpp>
 
-static const std::string OPENCV_WINDOW = "Open-CV display window";
+#include <ros/ros.h>
+#include <sensor_msgs/Image.h>
+#include <geometry_msgs/Pose.h>
+
+#include <gazebo_msgs/SetModelState.h>
+#include <reconstruction/ImageStamped.h>
+#include <cstdlib>
+
 using namespace std;
 
-int g_redratio; //threshold to decide if a pixel qualifies as dominantly "red"
+sensor_msgs::Image image_sub_;
+geometry_msgs::Pose image_pose_;
 
-class ImageConverter {
-    ros::NodeHandle nh_;
-    image_transport::ImageTransport it_;
-    image_transport::Subscriber image_sub_;
-    image_transport::Publisher image_pub_;
-
-public:
-
-    ImageConverter(ros::NodeHandle &nodehandle)
-    : it_(nh_) {
-        // Subscribe to input video feed and publish output video feed
-        image_sub_ = it_.subscribe("simple_camera/image_raw", 1,
-                &ImageConverter::imageCb, this);
-        image_pub_ = it_.advertise("/image_converter/output_video", 1);
-
-        cv::namedWindow(OPENCV_WINDOW);
-    }
-
-    ~ImageConverter() {
-        cv::destroyWindow(OPENCV_WINDOW);
-    }
-
-    //image comes in as a ROS message, but gets converted to an OpenCV type
-    void imageCb(const sensor_msgs::ImageConstPtr& msg); 
-    
-}; //end of class definition
-
-void ImageConverter::imageCb(const sensor_msgs::ImageConstPtr& msg){
-        cv_bridge::CvImagePtr cv_ptr; //OpenCV data type
-        try {
-            cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-        } catch (cv_bridge::Exception& e) {
-            ROS_ERROR("cv_bridge exception: %s", e.what());
-            return;
-        }
-        // look for red pixels; turn all other pixels black, and turn red pixels white
-        int npix = 0; //count the red pixels
-        int isum = 0; //accumulate the column values of red pixels
-        int jsum = 0; //accumulate the row values of red pixels
-        int redval, blueval, greenval, testval;
-        cv::Vec3b rgbpix; // OpenCV representation of an RGB pixel
-        //comb through all pixels (j,i)= (row,col)
-        for (int i = 0; i < cv_ptr->image.cols; i++) {
-            for (int j = 0; j < cv_ptr->image.rows; j++) {
-                rgbpix = cv_ptr->image.at<cv::Vec3b>(j, i); //extract an RGB pixel
-                //examine intensity of R, G and B components (0 to 255)
-                redval = rgbpix[2] + 1; //add 1, to avoid divide by zero
-                blueval = rgbpix[0] + 1;
-                greenval = rgbpix[1] + 1;
-                //look for red values that are large compared to blue+green
-                testval = redval / (blueval + greenval);
-                //if red (enough), paint this white:
-                if (testval > g_redratio) {
-                    cv_ptr->image.at<cv::Vec3b>(j, i)[0] = 255;
-                    cv_ptr->image.at<cv::Vec3b>(j, i)[1] = 255;
-                    cv_ptr->image.at<cv::Vec3b>(j, i)[2] = 255;
-                    npix++; //note that found another red pixel
-                    isum += i; //accumulate row and col index vals
-                    jsum += j;
-                } else { //else paint it black
-                    cv_ptr->image.at<cv::Vec3b>(j, i)[0] = 0;
-                    cv_ptr->image.at<cv::Vec3b>(j, i)[1] = 0;
-                    cv_ptr->image.at<cv::Vec3b>(j, i)[2] = 0;
-                }
-            }
-        }
-        //cout << "npix: " << npix << endl;
-        //paint in a blue square at the centroid:
-        int half_box = 5; // choose size of box to paint
-        int i_centroid, j_centroid;
-        double x_centroid, y_centroid;
-        if (npix > 0) {
-            i_centroid = isum / npix; // average value of u component of red pixels
-            j_centroid = jsum / npix; // avg v component
-            x_centroid = ((double) isum)/((double) npix); //floating-pt version
-            y_centroid = ((double) jsum)/((double) npix);
-            ROS_INFO("u_avg: %f; v_avg: %f",x_centroid,y_centroid);
-            //cout << "i_avg: " << i_centroid << endl; //i,j centroid of red pixels
-            //cout << "j_avg: " << j_centroid << endl;
-            for (int i_box = i_centroid - half_box; i_box <= i_centroid + half_box; i_box++) {
-                for (int j_box = j_centroid - half_box; j_box <= j_centroid + half_box; j_box++) {
-                    //make sure indices fit within the image 
-                    if ((i_box >= 0)&&(j_box >= 0)&&(i_box < cv_ptr->image.cols)&&(j_box < cv_ptr->image.rows)) {
-                        cv_ptr->image.at<cv::Vec3b>(j_box, i_box)[0] = 255; //(255,0,0) is pure blue
-                        cv_ptr->image.at<cv::Vec3b>(j_box, i_box)[1] = 0;
-                        cv_ptr->image.at<cv::Vec3b>(j_box, i_box)[2] = 0;
-                    }
-                }
-            }
-
-        }
-        // Update GUI Window; this will display processed images on the open-cv viewer.
-        cv::imshow(OPENCV_WINDOW, cv_ptr->image);
-        cv::waitKey(3); //need waitKey call to update OpenCV image window
-
-        // Also, publish the processed image as a ROS message on a ROS topic
-        // can view this stream in ROS with: 
-        //rosrun image_view image_view image:=/image_converter/output_video
-        image_pub_.publish(cv_ptr->toImageMsg());
-    }
+void imageCb(const sensor_msgs::Image msg){
+        image_sub_=msg;
+}
 
 int main(int argc, char** argv) {
-    ros::init(argc, argv, "image_saver");
-    ros::NodeHandle n; //        
-    ImageConverter ic(n); // instantiate object of class ImageConverter
-    //cout << "enter red ratio threshold: (e.g. 10) ";
-    //cin >> g_redratio;
-    g_redratio= 10; //choose a threshold to define what is "red" enough
-    ros::Duration timer(0.1);
-    double x, y, z;
-    while (ros::ok()) {
-        ros::spinOnce();
-        timer.sleep();
-    }
-    return 0;
+	ros::init(argc, argv, "image_saver");
+	ros::NodeHandle n; //        
+	ros::Duration timer(0.1);
+
+	//inst
+	reconstruction::ImageStamped msg;
+
+	ros::Subscriber my_sub_ = n.subscribe("simple_camera/image_raw", 1,imageCb);
+	ros::Publisher my_pub_ = n.advertise<reconstruction::ImageStamped>("image_stamped", 1);
+	ros::ServiceClient client = n.serviceClient<gazebo_msgs::SetModelState>("/gazebo/set_model_state"); 
+
+	geometry_msgs::Pose start_pose;
+	start_pose.position.x = 0;
+	start_pose.position.x = 0;
+	start_pose.position.z = 0;
+	start_pose.orientation.x = 0.0;
+	start_pose.orientation.y = 0.0;
+	start_pose.orientation.z = 0.0;
+	start_pose.orientation.w = 0.0;
+
+	geometry_msgs::Twist start_twist;
+	start_twist.linear.x = 0.0;
+	start_twist.linear.y = 0.0;
+	start_twist.linear.z = 0.0;
+	start_twist.angular.x = 0.0;
+	start_twist.angular.y = 0.0;
+	start_twist.angular.z = 0.0;
+
+	gazebo_msgs::SetModelState setmodelstate;
+	gazebo_msgs::ModelState modelstate;
+	
+	modelstate.model_name = "my_object"; 
+	modelstate.reference_frame = "world";
+	modelstate.twist = start_twist;
+	modelstate.pose = start_pose;
+	setmodelstate.request.model_state = modelstate;
+
+	float alpha;//orientation in x,y plane
+	float theta;
+	float radios;
+	float hight;
+	hight=0.3;
+	radios=0.5;
+	//theta=atan2(radios/hight);
+	alpha=0.0;
+	
+	//x=sin(alpha)
+	//y=cos(alpha)
+	while (ros::ok()) {
+
+		modelstate.twist = start_twist;
+		modelstate.pose = start_pose;
+		setmodelstate.request.model_state = modelstate;
+		image_pose_=start_pose;
+		if (client.call(setmodelstate)){
+			ROS_INFO("New Camera Pose!!!");
+			ROS_INFO("%f, %f, %f", modelstate.pose.position.x,modelstate.pose.position.y,modelstate.pose.position.y);
+		}else{
+			ROS_ERROR("Failed to call service ");
+		}
+		ros::spinOnce();
+		image_pose_=start_pose;
+		msg.header.stamp = ros::Time::now();
+		msg.header.frame_id = "/world";
+		msg.image=image_sub_;
+		msg.pose=image_pose_;
+		timer.sleep();
+	}
+	return 0;
 }
