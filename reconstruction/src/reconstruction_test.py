ccea3a4b67ffaf65e9a4715142f80a9d311887fd
SuragBalajepalli <surag.krishna@gmail.com>
2018-12-17 20:13:59 -0500

    backup before test

--- reconstruction/src/reconstruction_test.py
+++ reconstruction/src/reconstruction_test.py
@@ -4,7 +4,7 @@ import rospy
 import roslib
 import sensor_msgs
 #import opencv2
-import cv_bridge
+from cv_bridge import CvBridge
 import std_msgs
 import sys
 import cv2
@@ -18,7 +18,6 @@ import numpy as np
 imgs = []
 tf_mats = []
 pts = []
-
 g_seq = 0
 
 #camera params
@@ -28,6 +27,7 @@ f2 = 1.0
 pp2 = (0.0,0.0)
 #obviously wrong, need ammar to tell me
 
+K = np.eye(3)
 
 #tunable params
 MIN_MATCH_COUNT = 5
@@ -52,40 +52,47 @@ FIELDS = [
 
 
 
-def CallBack(self, data):
-	img = cv_bridge.imgmsg_to_cv2(data.image) #conversion from ROS msg format to cv2
+def CallBack(data):
+	global imgs
+	global tf_mats
+	br = CvBridge()
+	img = br.imgmsg_to_cv2(data.image) #conversion from ROS msg format to cv2
 	
 	origin = np.array([data.pose.position.x, data.pose.position.y, data.pose.position.z])
 
 
 	quat = data.pose.orientation
 
-	rot_mat = np.zeros(3,3)
+	tf_mat = np.zeros((4,4))
 
-	rot_mat[0,0] = 1 - 2 * quat.y ^ 2 - 2 * quat.z ^ 2 
-	rot_mat[0,1] = 2 * quat.x * quat.y - 2 * quat.z * quat.w
-	rot_mat[0,2] = 2 * quat.x * quat.z + 2 * quat.y * quat.w
+	tf_mat[0,0] = 1 - 2 * quat.y ** 2 - 2 ** quat.z ** 2 
+	tf_mat[0,1] = 2 * quat.x * quat.y - 2 * quat.z * quat.w
+	tf_mat[0,2] = 2 * quat.x * quat.z + 2 * quat.y * quat.w
 
-	rot_mat[1,0] = 2 * quat.x * quat.y + 2 * quat.z * quat.w
-	rot_mat[1,1] = 1 - 2 * quat.x ^ 2 - 2 * quat.z ^ 2
-	rot_mat[1,2] = 2 * quat.y * quat.z - 2 * quat.x * quat.w
+	tf_mat[1,0] = 2 * quat.x * quat.y + 2 * quat.z * quat.w
+	tf_mat[1,1] = 1 - 2 * quat.x ** 2 - 2 * quat.z ** 2
+	tf_mat[1,2] = 2 * quat.y * quat.z - 2 * quat.x * quat.w
 
-	rot_mat[2,0] = 2 * quat.x * quat.z - 2 * quat.y * quat.w 
-	rot_mat[2,1] = 2 * quat.y * quat.z + 2 * quat.x * quat.w
-	rot_mat[2,3] = 1 - 2 * quat.x ^ 2 - 2 * quat.y ^ 2
+	tf_mat[2,0] = 2 * quat.x * quat.z - 2 * quat.y * quat.w 
+	tf_mat[2,1] = 2 * quat.y * quat.z + 2 * quat.x * quat.w
+	tf_mat[2,2] = 1 - 2 * quat.x ** 2 - 2 * quat.y ** 2
 
-	proj_mat = np.block([[rot_mat, origin]])
+	tf_mat[0,3] = data.pose.position.x
+	tf_mat[1,3] = data.pose.position.y
+	tf_mat[2,3] = data.pose.position.z
+	tf_mat[3,3] = 1
 	
 	
 	# for dbg purp
-	print("recvd pose")
-	print(rot_mat)
-	print(origin)
-
 	imgs.append(img)
-	tf_mats.append(proj_mat)
+	tf_mats.append(tf_mat)
 
 def make3d(img1, tfMat1, img2, tfMat2):
+	print(tfMat1)
+	print(tfMat2)
+	projMat1 = np.dot(K, np.linalg.inv(tfMat1)[:3,:])
+	projMat2 = np.dot(K, np.linalg.inv(tfMat2)[:3,:])
+
 	#projection matrices are P = K * [R|t] where K is a 3x3 of the intrinsic params of each camera
 	res_img_sift1 = img1
 	res_img_sift2 = img2
@@ -97,7 +104,7 @@ def make3d(img1, tfMat1, img2, tfMat2):
 	# img_sift1 = cv2.drawKeyPoints(img1,kp1,None,flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
 	#implementing FLANN matching of sift features
-	flann = cv2.FlannBasedMatches(index_params,search_params)
+	flann = cv2.FlannBasedMatcher(index_params,search_params)
 	matches = flann.knnMatch(des1,des2,k=K_neighbors)
 
 	#storing good matches
@@ -108,46 +115,48 @@ def make3d(img1, tfMat1, img2, tfMat2):
 			good.append(m)
 
 	if len(good) > MIN_MATCH_COUNT:
-		src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(2,-1)
-		dst_pts = np.float32([kp2[m.queryIdx].pt for m in good]).reshape(2,-1)
+		src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1,1,2)
+		dst_pts = np.float32([kp2[m.queryIdx].pt for m in good]).reshape(-1,1,2)
 
-		src_pts_norm = cv2.undistortPoints(src_pts, cameraMatrix=K1,distCoeffs=None)
-		dst_pts_norm = cv2.undistortPoints(dst_pts, cameraMatrix=K2,distCoeffs=None)
+		src_pts_norm = cv2.undistortPoints(src_pts, cameraMatrix=K,distCoeffs=None)
+		dst_pts_norm = cv2.undistortPoints(dst_pts, cameraMatrix=K,distCoeffs=None)
 
-		E, mask = cv2.findEssentialMat(src_pts_norm,dst_pts_norm,focal=1.0, pp=(0.,0.),method=cv2.RANSAC,prob=0.999,threshold=3.0)
+		E, mask = cv2.findEssentialMat(src_pts_norm,dst_pts_norm,focal=f1, pp=pp1,method=cv2.RANSAC,prob=0.999,threshold=3.0)
 		points,R,t,mask = cv2.recoverPose(E,src_pts_norm,dst_pts_norm)
 
 		#compare this tf to gazebo reported tf. any way to use these as estimates?
 		M_src = np.hstack((R,t))
 		M_dst = np.hstack((np.eye(3,3),np.zeros((3,1))))
 		
-		P_src = np.dot(K_src,M_src)
-		P_dst = np.dot(K_dst,M_dst)		
+		P_src = np.dot(K,M_src)
+		P_dst = np.dot(K,M_dst)		
 
 		points3d = cv2.triangulatePoints(P_src,P_dst,src_pts,dst_pts)
-
+		points3d = cv2.triangulatePoints(projMat1,projMat2,src_pts,dst_pts)
 		points3d /= points3d[3]
-		return points3d
+		points_world = np.dot((np.linalg.inv(tfMat1)), points3d)[:3,:]
+		return points_world
 	else:
 		return 0
 
-def populatePointCloud(points3d):
+def populatePointCloud(points):
 	header = Header()
 	#header.seq = g_seq
 	#g_seq += 1
 	header.frame_id = 'map'
 	header.stamp = rospy.Time.now()
-	cloud = pc2.create_cloud_xyz32(header,points3d)
+	cloud = pc2.create_cloud_xyz32(header,points)
 	return cloud
 
 
 def main():
+	global pts
 	rospy.init_node('image_feature', anonymous=True)
-	sub = rospy.Subscriber("image_stamped", ImageStamped, queue_size = 1)
+	sub = rospy.Subscriber("image_stamped", ImageStamped, CallBack)
 	pub = rospy.Publisher("reconstructed_pts", PointCloud2, queue_size = 1)
-
+	rospy.sleep(1)
 	#for dbg 
-	pts.append([1.0,2.0,3.0])
+	
 
 	prev_cnt = 0
 	while not rospy.is_shutdown():
@@ -155,19 +164,21 @@ def main():
 		tf_mats_np = np.array(tf_mats)
 		new_cnt = imgs_np.shape[0]
 		
-		# TODO
-		for i in range(prev_cnt, new_cnt):
-			img1 = imgs_np[i]
-			img2 = imgs_np[i+1]
-			proj_mat1 = tf_mats_np[i]
-			proj_mat2 = tf_mats_np[i+1]
-			
-			recon_pts = make3d(img1,proj_mat1,img2,proj_mat2)
-			if not recon_pts == 0:
-				pts.append(recon_pts[:,0:2])
 		
-		cloud = populatePointCloud(pts) 
-		pub.publish(cloud)
+		for i in range(prev_cnt+1, new_cnt):
+			img1 = imgs_np[i-1]
+			img2 = imgs_np[i]
+			tf_mat1 = tf_mats_np[i-1]
+			tf_mat2 = tf_mats_np[i]
+			print("inloop")
+			recon_pts = make3d(img1,tf_mat1,img2,tf_mat2)
+			if not len(recon_pts) == 0:
+				new_pts = np.asarray(recon_pts,np.float32)
+				print(new_pts.shape)
+				for i in range(new_pts.shape[1]):
+					pts.append([new_pts[0,i],new_pts[1,i],new_pts[2,i]])
+			cloud = populatePointCloud(pts) 
+			pub.publish(cloud)
 		prev_cnt += new_cnt
 
 
---
+++ reconstruction/src/sender_test.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+
+import rospy
+import roslib
+import sensor_msgs
+#import opencv2
+from cv_bridge import CvBridge
+import std_msgs
+import sys
+import cv2
+import sensor_msgs.point_cloud2 as pc2
+from sensor_msgs.msg import PointCloud2, PointField
+from reconstruction.msg import ImageStamped
+from std_msgs.msg import Header
+import numpy as np
+from geometry_msgs.msg import Pose
+
+def main():
+	rospy.init_node('image_sender', anonymous=True)
+	pub = rospy.Publisher("image_stamped", ImageStamped, queue_size = 1)
+	br = CvBridge()
+	pose1 = Pose()
+	pose2 = Pose()
+	img1 = cv2.imread('image_1.png')
+	img2 = cv2.imread('image_2.png')
+
+	img1_ros = br.cv2_to_imgmsg(img1)
+	img2_ros = br.cv2_to_imgmsg(img2)
+
+	pose1.position.x = 0.433012783527
+	pose1.position.y = -0.249999880791
+	pose1.position.z = 0.300000011921
+	pose1.orientation.w = -0.226004580276
+	pose1.orientation.x = 0.224733660268
+	pose1.orientation.y = -0.0251856589319
+	pose1.orientation.z = -0.947513796322
+
+	pose2.position.x = -0.469846427441
+	pose2.position.y = 0.171009778976
+	pose2.position.z = 0.300000011921
+	pose2.orientation.w = -0.955021155296
+	pose2.orientation.x = -0.0593451678968
+	pose2.orientation.y = -0.213517445104
+	pose2.orientation.z = 0.197035643023
+
+	msg1 = ImageStamped()
+	msg2 = ImageStamped()
+
+	msg1.image = img1_ros
+	msg1.pose = pose1
+
+	msg2.image = img2_ros
+	msg2.image = img2_ros
+
+	while not rospy.is_shutdown():
+		pub.publish(msg1)
+		rospy.sleep(0.5)
+		pub.publish(msg2)
+
+if __name__ == '__main__':
+	main()
