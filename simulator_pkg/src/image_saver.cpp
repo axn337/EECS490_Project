dc34bf49d63d3f0181c8e165aa368b3e74399aae
Ammar Nahari <axn337@case.edu>
2018-12-18 00:29:47 -0500

    Update image_saver.cpp

--- simulator_pkg/src/image_saver.cpp
+++ simulator_pkg/src/image_saver.cpp
@@ -1,14 +1,16 @@
+// Ammar Nahari- axn337
+// last edit 12/18/2018
 
-#include <ros/ros.h>
-#include <cmath>
-#include <sensor_msgs/Image.h>
-#include <geometry_msgs/Pose.h>
+#include <ros/ros.h> //main ros library
+#include <cmath> //for sqrt and pow functions
+#include <sensor_msgs/Image.h>  // the type of message for image data
+#include <geometry_msgs/Pose.h> //pose message data type
 
-#include <gazebo_msgs/SetModelState.h>
-#include <reconstruction/ImageStamped.h>
+#include <gazebo_msgs/SetModelState.h> //command publisher data type
+#include <reconstruction/ImageStamped.h> // Image stamped data type that has the image and the pose
 #include <cstdlib>
 
-#include <Eigen/Geometry>
+#include <Eigen/Geometry> // for quaternion orientation
 #include <Eigen/Dense>
 
 using namespace Eigen;
@@ -16,22 +18,26 @@ using namespace Eigen;
 sensor_msgs::Image image_sub_;
 geometry_msgs::Pose image_pose_;
 
+//call back function for importing sensor value and saving it in image_sub_
 void imageCb(const sensor_msgs::Image msg){
         image_sub_=msg;
 }
 
 int main(int argc, char** argv) {
-	ros::init(argc, argv, "image_saver");
-	ros::NodeHandle n; //        
-	ros::Duration timer(0.1);
+	ros::init(argc, argv, "image_saver"); //ros node initializer
+	ros::NodeHandle n; //node handle variable initializer
+	ros::Duration timer(0.1); // updae rate set
 
 	//inst
-	reconstruction::ImageStamped msg;
-
-	ros::Subscriber my_sub_ = n.subscribe("simple_camera/image_raw", 1,imageCb);
+	reconstruction::ImageStamped msg; 
+	//subscribing to the image data
+	ros::Subscriber my_sub_ = n.subscribe("simple_camera/image_raw", 1,imageCb); 
+	//publishing images with poses 
 	ros::Publisher my_pub_ = n.advertise<reconstruction::ImageStamped>("image_stamped", 1);
+	//for commanding camera poses
 	ros::ServiceClient client = n.serviceClient<gazebo_msgs::SetModelState>("/gazebo/set_model_state"); 
-
+	
+	// initializing camera pose variables and model state msg constructer
 	//geometry_msgs::Pose start_pose;
 	image_pose_.position.x = 0;
 	image_pose_.position.x = 0;
@@ -57,23 +63,25 @@ int main(int argc, char** argv) {
 	modelstate.twist = start_twist;
 	modelstate.pose = image_pose_;
 	setmodelstate.request.model_state = modelstate;
-
+	
 	float alpha=0.0;//orientation in x,y plane
 	float theta;//the angle of the victor pointing towards the camera
 	float d; //the length of segment from origin to camera origin
 	float dxy=0.5; // the radios projection in xy-plane
 	float hight=0.3; //hight of camera
-	float x,y,z,roll,pitch,yaw;
+	float x,y,z,roll,pitch,yaw; //6 degrees of freedom transformation and orientation
 
 	d=sqrt(pow(hight,2)+pow(dxy,2));
 	theta=atan2(hight,dxy);
 	
+	//initializing quaternion variable
 	Quaternionf q;
 
 	while (ros::ok()) {
-
+		//incrementing alpha by 10 degrees in each iteration
 		alpha=alpha+0.174533;
 
+		//Setting the pose values 
 		x=dxy*cos(alpha);
 		y=dxy*sin(alpha);
 		z=hight;
@@ -81,11 +89,12 @@ int main(int argc, char** argv) {
 		pitch=(sin(alpha)*theta)+0.958;
 		yaw=alpha+(1.5708)+1.586;
 
-	
+		//converting to quaternion representation
 		q = AngleAxisf(roll, Vector3f::UnitX())
 		    * AngleAxisf(pitch, Vector3f::UnitY())
 		    * AngleAxisf(yaw, Vector3f::UnitZ());
-
+		
+		// constructing the client service request
 		image_pose_.position.x = x;
 		image_pose_.position.y = y;
 		image_pose_.position.z = z;
@@ -98,7 +107,8 @@ int main(int argc, char** argv) {
 		modelstate.twist = start_twist;
 		modelstate.pose = image_pose_;
 		setmodelstate.request.model_state = modelstate;
-
+		
+		//calling the service
 		if (client.call(setmodelstate)){
 			ROS_INFO("New Camera Pose!!!");
 			ROS_INFO("Position: \n %f, %f, %f", modelstate.pose.position.x, modelstate.pose.position.y, modelstate.pose.position.z);
@@ -106,13 +116,16 @@ int main(int argc, char** argv) {
 		}else{
 			ROS_ERROR("Failed to call service ");
 		}
+		//checking the callback function and updating the image variable
 		ros::spinOnce();
-
+		
+		//publishing the image_stamped topic
 		msg.header.stamp = ros::Time::now();
 		msg.header.frame_id = "/world";
 		msg.image=image_sub_;
 		msg.pose=image_pose_;
 		timer.sleep();
 	}
+	//move here only if interrupted 
 	return 0;
 }
